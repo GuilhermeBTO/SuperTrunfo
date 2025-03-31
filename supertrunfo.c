760d9807e0660b24f6ed029b7c66c747bab6606f
guiherme <guilherme190301@gmail.com>
2025-03-30 22:55:36 -0300

    supertrunfo

---
+++ supertrunfo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Dados para a Carta 1 
+    char estado1[] = "Amazonia";
+    char codigo_carta1[] = "A01";
+    char nome_cidade1[] = "Manaus";
+    int populacao1 = 2.064 milhoes;
+    float area1 = 706.8;
+    float pib1 = 10000.0;
+    int pontos_turisticos1 = 15;
+
+    // Dados para a Carta 2 
+    char estado2[] = "Boa Vista";
+    char codigo_carta2[] = "A02";
+    char nome_cidade2[] = "Roraima";
+    int populacao2 = 470169;
+    float area2 = 224.301;
+    float pib2 = 18203;
+    int pontos_turisticos2 = 12;
+
+    void ExibirCarta
